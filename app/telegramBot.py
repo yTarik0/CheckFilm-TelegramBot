4aa28d0599ae5437e4167866c7d2612491b74ed2
yTarik0 <119168352+yTarik0@users.noreply.github.com>
2024-02-07 15:54:38 +0100

    few updates

--- app/telegramBot.py
+++ app/telegramBot.py
@@ -23,32 +23,36 @@ commandlist = ["/commandlist", "/checkmovie","/usage","/help"]
 # Ein Dictionary, um den Zustand jedes Benutzers zu speichern
 user_expected_input = {}
 
+# create list from every Message-ID --> to clear chat
+message_ids = []
 
-# Check Movie Search Commannd --> funcitons
+# add messages to List 
+def add_message_id(message_id):
+    message_ids.append(message_id)
+
+
+# Check Movie Search Commannds --> funcitons
 
 
 # function checkMovie
 def checkMovie(message):
     user_expected_input[message.chat.id] = True
-    bot.reply_to(message, "Please type a movie name")
+    reply = bot.reply_to(message, "Please type a movie name")
+    add_message_id(reply.message_id)
 
 # message handler --> respond like telegrambot
 @bot.message_handler(func=lambda message: message.chat.id in user_expected_input)
 def handle_movie_name(message):
     if user_expected_input.pop(message.chat.id, None):
         film_name = message.text
-        # Hier rufen Sie Ihre Funktion auf, um die Verfügbarkeit des Films zu überprüfen
-        # Angenommen, `main` ist Ihre Funktion, die die Verfügbarkeit prüft und einen String zurückgibt
         result = main(film_name)
 
         markup = InlineKeyboardMarkup()
         search_button = InlineKeyboardButton("📊 Back to Menu", callback_data='menu_button')
         markup.add(search_button)
 
-        # Antwort mit dem Ergebnis und einem Inline-Keyboard
-        #bot.reply_to(message, f"Checking where Movie - <b>{film_name.upper()}</b> available", parse_mode='HTML')
-        bot.reply_to(message, result, reply_markup=markup, parse_mode='HTML')
-
+        reply = bot.reply_to(message, result, reply_markup=markup, parse_mode='HTML')
+        add_message_id(reply.message_id)
 
 # checkMovie Command
 @bot.message_handler(commands=["checkmovie"])
@@ -57,10 +61,11 @@ def command_handler(message):
 
 
 # Commandlist Command
-@bot.message_handler(commands=["commandlist", "help"])
+@bot.message_handler(commands=["help"])
 def get_commmandlist(message):
     commands_str = "\n".join(commandlist)
-    bot.reply_to(message, f"<b>Available Commands</b>:\n{commands_str}",  parse_mode='HTML')
+    reply = bot.reply_to(message, f"<b>Available Commands</b>:\n{commands_str}",  parse_mode='HTML')
+    add_message_id(reply.message_id)
 
 
 # Usage Command
@@ -75,17 +80,17 @@ def echo_all( message):
     local_command_list = ["/hello", "/checkmovie", "/usage", "/commandlist"]
     is_command = any(message.text.startswith(command) for command in local_command_list)
     if not is_command:
+
         # Inline-Keyboard erstellen
         markup = InlineKeyboardMarkup()
        
-            
         # Button der über die gesamte Breite geht
         search_button = InlineKeyboardButton("🔎 Search for a Movie", callback_data='search_button')
         markup.add(search_button)  # Fügt den Button in seiner eigenen Reihe hinzu
             
         #  Buttons nebeneinander in einer neuen Reihe
         usage_button = InlineKeyboardButton("❔ Usage", callback_data='usage_button')
-        close_button = InlineKeyboardButton("❌ Close", callback_data='close_button')
+        close_button = InlineKeyboardButton("❌ Clear Chat", callback_data='clear_button')
             
         markup.row(usage_button, close_button)  # Fügt beide Buttons in der gleichen Reihe hinzu
 
@@ -97,8 +102,8 @@ def echo_all( message):
         mention = f'<a href="tg://user?id={message.from_user.id}">{user_first_name}</a>'
             
         # Nachricht mit Inline-Keyboard senden
-        bot.reply_to(message, f"<b>Hello {mention}</b> 👋,\nPlease run one of the <b>Commands</b> by clicking on the buttons or by typing <b>/commandlist</b> in the chat\nto get a <b>list of all commands</b>", reply_markup=markup, parse_mode='HTML')
-
+        reply = bot.reply_to(message, f"<b>Hello {mention}</b> 👋,\nPlease run one of the <b>Commands</b> by clicking on the buttons or by typing <b>/commandlist</b> in the chat\nto get a <b>list of all commands</b>", reply_markup=markup, parse_mode='HTML')
+        add_message_id(reply.message_id)
 
 # Button-Callbacks --> if button pressed:
 @bot.callback_query_handler(func=lambda call: True)
@@ -107,16 +112,32 @@ def handle_query(call):
         # Benachrichtigung Oben Response Pop-Up
         bot.answer_callback_query(call.id, "Usage: Use /checkmovie [movie-name] to search for a movie's availability.")
         # Text-Message Response
-        bot.send_message(call.message.chat.id, "Usage: Use /checkmovie [movie-name] to search for a movie's availability.\nPlease make sure you make no mistakes when typing the movie name")
+        reply = bot.send_message(call.message.chat.id, "Usage: Use /checkmovie [movie-name] to search for a movie's availability.\nPlease make sure you make no mistakes when typing the movie name")
+        add_message_id(reply.message_id)
 
     if call.data == "search_button":
        forceReply_markUp = telebot.types.ForceReply(selective=True)
-       bot.send_message(call.message.chat.id, "Please type the movie name", reply_markup=forceReply_markUp)
-       bot.answer_callback_query(call.id)
-
+       bot.answer_callback_query(call.id,"Type a Movie Name in the Chat")
+       checkMovie(call.message)
+       
     if call.data == "support_button":
         bot.answer_callback_query(call.id, "Contact CheckMovieSOL on Twitter")
-        bot.send_message(call.message.chat.id, "If there are any problems let us know\n📧 Contact: https://twitter.com/CheckMovieSOL")
+        reply = bot.send_message(call.message.chat.id, "If there are any problems let us know\n📧 Contact: https://twitter.com/CheckMovieSOL")
+        add_message_id(reply.message_id)
 
+
+    if call.data == "clear_button":
+        bot.answer_callback_query(call.id, "Chat successfully cleared!")
+        for message_id in message_ids:
+            try:
+                bot.delete_message(chat_id=call.message.chat.id, message_id=message_id)
+            except Exception as e:
+                # Debug if error
+                print(f"Fehler beim Löschen der Nachricht {message_id}: {e}")
+        # clear list after, chat cleared!
+        message_ids.clear()
+
+    if call.data == "menu_button":
+        echo_all(call.message)
 # Am Ende des Codes damit alle Befehle gesynced sind
 bot.polling(none_stop=True) # none_stop=True --> wenn fehler kommt damit nicht stopt
