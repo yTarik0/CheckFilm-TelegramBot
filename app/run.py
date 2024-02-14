89d4bdc1583c05a02f27366362d6d310e5064fe8
yTarik0 <119168352+yTarik0@users.noreply.github.com>
2024-02-14 21:01:56 +0100

    Update run.py

--- app/run.py
+++ app/run.py
@@ -1,8 +1,9 @@
 from telegramBot import bot
+from logsNotification import notify_logsUpdate
 
 def run():
     try:
-        # Bot Starting..
+        # Bot Starting....
         print("🛫 Telegram-Bot: CheckMovie is running...🛫")
         bot.polling(none_stop=True) # none_stop=True --> wenn fehler kommt damit nicht stopt
     except Exception as e:
@@ -10,3 +11,5 @@ def run():
 
 if __name__ == "__main__":
     run()
+    notify_logsUpdate()
+    
