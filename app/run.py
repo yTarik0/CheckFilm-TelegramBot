c91eb0bfad89747d7cd8178c3af0ad34942085c4
yTarik0 <119168352+yTarik0@users.noreply.github.com>
2024-02-14 12:36:06 +0100

    Create run.py

---
+++ app/run.py
@@ -0,0 +1,12 @@
+from telegramBot import bot
+
+def run():
+    try:
+        # Bot Starting..
+        print("🛫 Telegram-Bot: CheckMovie is running...🛫")
+        bot.polling(none_stop=True) # none_stop=True --> wenn fehler kommt damit nicht stopt
+    except Exception as e:
+        print(f"Ein Fehler ist aufgetreten: {e}")
+
+if __name__ == "__main__":
+    run()
